4fada0234690f4754f8223d0d47f44f76966e641
Richard Li <richardli125@gmail.com>
2019-06-12 21:08:53 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+  ("Mr. Chocolate", 4, "M", "Brown", "Fun", true),
+  ("Rowdy", 10, "M", "White", "Trouble", true),
+  ("Tabitha", 20, "F", "Brown", "Nice", true),
+  ("Sergeant Brown", 45, "M", "Black", "Brave", true),
+  ("Melissa", 12, "F", "Pink", "Serious", true),
+  ("Grinch", 28, "M", "Green", "Secretly Nice", true),
+  ("Wendy", 41, "F", "Pink", "Nice", false),
+  (NULL, 4, "M", "Brown", "Fun", true);
